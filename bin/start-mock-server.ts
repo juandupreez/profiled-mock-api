1509fa6e792393464a466b1974d0ca82ed9ecd18
jdupreez <jdupreez@mit.edu>
2022-10-19 07:40:08 +0200

    Fix mock server start

--- bin/start-mock-server.ts
+++ bin/start-mock-server.ts
@@ -1,7 +1,11 @@
 import { MockApiServer } from "../src/MockApiServer"
+import { GlobalLogger, LogLevel } from "../src/util/Logger"
+
+GlobalLogger.getInstance().logLevel = LogLevel.ALL
 
 const mockApiServer: MockApiServer = new MockApiServer({
     port: 3002,
+    initialActiveProfile: 'default',
     profiles: {
         default: {
             responseFileBasePath: "./resources/default"
